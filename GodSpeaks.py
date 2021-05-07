886e501b975b5cc3d9d9e1a02db2608339b8a519
debaet <81795907+debaet@users.noreply.github.com>
2021-05-07 12:15:20 -0700

    loop , input
    edited to make code more user friendly and allows users to  keep receiving text

--- GodSpeaks.py
+++ GodSpeaks.py
@@ -1,5 +1,7 @@
 import random
 
+keywords = ["yes", "yep", "sure","yeah","yuh","Yes","Yep","Sure","Yeah","Yuh"]
+
 def main():
     with open("Happy.TXT") as f:
         gdWrds = f.read().splitlines()
@@ -13,4 +15,18 @@ def main():
     wrds = random.choice(gdWrds)
     print(wrds + ".\n")
 
-main()
+
+
+
+while True:
+    main()
+    for x in range(0, 3):
+        main()
+
+    var = input('display more?')
+
+
+    if any(keyword in var for keyword in keywords):
+        main()
+    else:
+        exit()
